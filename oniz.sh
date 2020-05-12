a4a49ab723b4932bcdefe98b4fbd72488e5e2238
Roniz <50039352+Mr-R0N1Z@users.noreply.github.com>
2020-05-12 12:32:19 +0700

    Add files via upload

---
+++ oniz.sh
@@ -0,0 +1,18 @@
+#usr/bin/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+# yang Recode Besok  Mati Amin
+echo $pur
+echo " _____  ______ _______ ______  _______
+   |   |  ____ |  |  | |_____] |______
+ __|__ |_____| |  |  | |_____] |      
+                                      "
+echo $me"Youtube:"$pu" RONIZ"
+echo $ku"Github:"$pu" https://github.com/Mr-R0n1z"
+echo " "
